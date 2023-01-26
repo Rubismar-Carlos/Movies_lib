870dfab69670401434d0fbcb18dd63da0166aaf5
Rubismar Carlos <100701721+Rubismar-Carlos@users.noreply.github.com>
2023-01-26 20:02:33 -0300

    movies_lib

---
+++ movies_lib/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+
+    const nome = "Rubismar Carlos"
+
+  return (
+    <footer>
+        <p>Site desenvolvido por {nome}</p>
+    </footer>
+  )
+}
+
+export default Footer
